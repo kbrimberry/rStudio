486de49cab99990ffbbfb112d37f11079acf9456
kbrimberry <kbrimberry@harveytool.com>
2014-11-05 11:00:06 -0500

    Added a new line of text to modify the elements
    Test methodology...again...trying to figure this Git Out

---
+++ global.R
@@ -0,0 +1,11 @@
+alpha <- function(x,y){
+  z <- x*y
+  return(z)
+}
+
+print <- function(input){
+  print(alpha(1,2))
+}
+
+#Appending Changes to the primary hashfile
+#Making the secondary changes 11/5/2014
---
+++ globalV1.0.1.R
@@ -0,0 +1,13 @@
+alpha <- function(x,y){
+  z <- x*y
+  return(z)
+}
+
+print <- function(input){
+  print(alpha(1,2))
+}
+
+#Appending Changes to the primary hashfile
+#Making the secondary changes 11/5/2014
+
+#Made Alternative Changes to Test the Transfer of File Handles
